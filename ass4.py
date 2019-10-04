ab7147196a8ba788b2204d4add5b39ed3c713050
MariaTasova <55009548+MariaTasova@users.noreply.github.com>
2019-10-04 21:31:34 +0900

    Update ass4.py

--- ass4.py
+++ ass4.py
@@ -2,12 +2,15 @@ def is_prime(n):
     if n <= 1:
         return False
     else:
-        for i in range(2, n):
+        for i in range(2, n//2 + 1):
             if n % i == 0:
                 return False
         return True
 
-print(is_prime(14))
+print(is_prime(144))
+
+#You do not actually need to go all the way up to n if you want to check if n is a prime number. You can dramatically reduce the tests and only check from 2 to √(n)
+
 
 
 

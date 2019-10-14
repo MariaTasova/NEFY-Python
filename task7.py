be5389c467f0a96680dd01c7fbabcfefc7c6ac86
MariaTasova <55009548+MariaTasova@users.noreply.github.com>
2019-10-14 20:04:24 +0900

    Delete task7.py

--- task7.py
+++
@@ -1,9 +0,0 @@
-def get_three_sum(nums, target):
-    for i in range(0, len(nums)-2):
-        for k in range(i+1, len(nums)-1):
-            for j in range(k+1, len(nums)):
-                if nums[i] + nums[k] + nums[j] == target:
-                    return i, k, j
-    return "None"
-
-print(get_three_sum([2, 7, 11, 15], target=24))

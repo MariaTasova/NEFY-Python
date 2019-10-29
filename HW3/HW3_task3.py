c9c9321c05de88fb50586c455c0290d993a7be6a
MariaTasova <55009548+MariaTasova@users.noreply.github.com>
2019-10-29 18:04:52 +0900

    Update HW3_task3.py

--- HW3/HW3_task3.py
+++ HW3/HW3_task3.py
@@ -9,11 +9,3 @@ for i, line in enumerate(gen):
     print(line)
     if i > 50:
         break
-
-print(list(map(len, rows_generator("/home/maria/Документы/test3.txt"))))
-
-print(list(filter(lambda x: x == 'NOD19', rows_generator("/home/maria/Документы/test3.txt"))))
-
-
-
-
